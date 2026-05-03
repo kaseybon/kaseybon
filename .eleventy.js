aa2edb589fcd35fe8533ff2203c906aedbe7d285
Kasey Bonifacio <kaseybon@gmail.com>
2026-05-03 11:14:41 -0400

    feat: add words page

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,10 @@
+const yaml = require("js-yaml");
+
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ "src/public": "." });
   eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+  eleventyConfig.addDataExtension('yaml', contents => yaml.safeLoad(contents));
+
 
   return {
     dir: {
