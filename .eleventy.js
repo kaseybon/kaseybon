9e8004ac46fc34461d70d97113618106fa3d08b0
Kasey Bonifacio <kaseybon@gmail.com>
2026-05-03 11:34:01 -0400

    chore: update build

--- .eleventy.js
+++ .eleventy.js
@@ -3,7 +3,7 @@ const yaml = require("js-yaml");
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ "src/public": "." });
   eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
-  eleventyConfig.addDataExtension('yaml', contents => yaml.safeLoad(contents));
+  eleventyConfig.addDataExtension('yaml', contents => yaml.load(contents));
 
 
   return {
